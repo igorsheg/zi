a6277fc55fb13e86b78f2aba87f2d56e8f899d7d
Igor Shegolev <igorsheg@gmail.com>
2026-05-05 15:26:39 +0300

    fix(website): allow release output reuse
    Session-Id: 1dfec70a-18f9-4cc9-a513-840aa9a8720e

--- website/build.zig
+++ website/build.zig
@@ -7,6 +7,7 @@ pub fn build(b: *std.Build) void {
     const zine_site = b.addSystemCommand(&.{
         "zine",
         "release",
+        "--force",
         b.fmt("--output={s}", .{b.install_prefix}),
     });
 
