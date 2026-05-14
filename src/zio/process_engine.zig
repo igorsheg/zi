5a223918e3182b1188159f187e0a8e29efa73653
Igor Shegolev <igorsheg@gmail.com>
2026-05-14 12:09:39 +0300

    refactor(zio): remove stale kqueue process engine

--- src/zio/process_engine.zig
+++ src/zio/process_engine.zig
@@ -1,12 +1,10 @@
 const builtin = @import("builtin");
 
 const blocking = @import("process_engine_blocking.zig");
-const kqueue = @import("process_engine_kqueue.zig");
 const linux = @import("process_engine_linux.zig");
 const types = @import("process_engine_types.zig");
 
 const backend = switch (builtin.os.tag) {
-    .macos, .ios, .visionos => kqueue,
     .linux => linux,
     else => blocking,
 };
